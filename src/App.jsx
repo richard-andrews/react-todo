d9f5ea7e065abf2b3ffeb4774a88479007d91898
Richard Andrews <richardxndrews@gmail.com>
2023-12-24 16:20:22 +0000

    add static todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import "./styles.css"
 
 export default function App() {
@@ -10,6 +11,16 @@ export default function App() {
         </div>
         <button className="btn">Add</button>
       </form>
+      <h1 className="header">ToDo List</h1>
+        <ul className="list">
+          <li>
+            <label>
+              <input type="checkbox" />
+              Item 1
+            </label>
+            <button className="btn btn-danger">Delete</button>
+          </li>
+        </ul>
     </>
   )
 }
